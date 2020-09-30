67e5a489952c76438d23ab08e0918db324a0c948
Karun Kanda <kkanda900@gmail.com>
2020-09-30 17:01:30 -0400

    updated Controller.java

--- src/app/SongLib.java
+++ src/app/SongLib.java
@@ -4,16 +4,23 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import view.Controller;
 
 public class SongLib extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		Parent rt = FXMLLoader.load(getClass().getResource("/view/Layout.fxml"));
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("/view/Layout.fxml"));
+		AnchorPane root = (AnchorPane)loader.load();
+		
+		Controller controller = loader.getController();
+		controller.start(primaryStage);
+		
 		primaryStage.setTitle("Song Library");
-		primaryStage.setScene(new Scene(rt, 747, 400));
+		primaryStage.setScene(new Scene(root, 747, 400));
 		primaryStage.setResizable(false);
 		primaryStage.show();
 	}
--- src/view/Controller.java
+++ src/view/Controller.java
@@ -6,22 +6,37 @@ package view;
 
 import javafx.event.ActionEvent;
 
+import java.net.URL;
+import java.util.Optional;
+import java.util.ResourceBundle;
+
 import com.sun.glass.ui.Accessible.EventHandler;
 
 import app.Song;
 import javafx.beans.Observable;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputDialog;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Controller extends ActionEvent {
-	@FXML ListView<Song> songPlayList;
+	
+	/*
+	 * Here the full list of widgets used in the FXML will go we so 
+	 * can reference them when modifying anything in the project.
+	 */
+	@FXML ListView songPlayList;
 	@FXML Button edit;
 	@FXML Button add;
 	@FXML Button delete;
@@ -30,7 +45,12 @@ public class Controller extends ActionEvent {
 	@FXML TextField YearBox;
 	@FXML TextField AlbumBox;
 	
+	/*
+	 * ObservableList to add items to ListView which will take a 
+	 * items of the Song Object that we created.
+	 */
 	private ObservableList<Song> songs = FXCollections.observableArrayList();
+	
 	
 	@FXML
 	private void addButtonAction(ActionEvent event) {
@@ -39,8 +59,20 @@ public class Controller extends ActionEvent {
 		String album = AlbumBox.getText();
 		String year = YearBox.getText();
 		Song new_song = new Song(song, artist, album, Integer.parseInt(year));
-		songs.add(new_song);
-		songPlayList = new ListView<Song>(songs);
-		System.out.println(songPlayList);
 	}
+	
+	public void start(Stage primaryStage) {
+		songs.add(new Song("Hello", "Adele", "25", 2015));
+		songs.add(new Song("Circles", "Mac Miller", "Circles", 2020));
+		songs.add(new Song("No Role Models", "J Cole", "2014 Forest Hill Drive", 2014));
+		songPlayList.setItems(songs);
+		songPlayList.getSelectionModel().select(0);
+		if(songPlayList != null) {
+			System.out.println(songPlayList.getItems());
+		}
+	}
+	
+	
+	
+		
 }
