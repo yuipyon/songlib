4894f0b37a49d32e0aa45b66daa8e2776b99645f
Karun Kanda <36608879+KKanda900@users.noreply.github.com>
2020-10-01 18:12:03 -0400

    Update Song.java

--- src/app/Song.java
+++ src/app/Song.java
@@ -1,9 +1,10 @@
+//Yulin Ni (yn140) and Karun Kanda (kk951)
 package app;
 
 public class Song {
-	String name, artist, album; int year;
+	String name, artist, album, year;
 	
-	public Song(String name, String artist, String album, int year) {
+	public Song(String name, String artist, String album, String year) {
 		this.name = name;
 		this.artist = artist;
 		this.album = album;
@@ -13,12 +14,12 @@ public class Song {
 	public String getName() {return name;}
 	public String getArtist() {return artist;}
 	public String getAlbum() {return album;}
-	public int getYear() {return year;}
+	public String getYear() {return year;}
 	
 	public void setName(String name) {this.name=name;}
 	public void setArtist(String artist) {this.artist=artist;}
 	public void setAlbum(String album) {this.album=album;}
-	public void setYear(int year) {this.year=year;}
+	public void setYear(String year) {this.year=year;}
 	
 	public String toString() {
 		return name + " " + "-" + " " + artist;
