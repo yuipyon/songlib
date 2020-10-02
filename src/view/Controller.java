abf3e46fdb486f5fe57332151e507d6e35ace03e
yuipyon <53494094+yuipyon@users.noreply.github.com>
2020-10-02 16:10:05 -0400

    bleh

--- src/view/Controller.java
+++ src/view/Controller.java
@@ -8,30 +8,20 @@ package view;
 
 import javafx.event.ActionEvent;
 
-
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Optional;
-import java.util.ResourceBundle;
 import java.util.Scanner;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
-import com.sun.glass.ui.Accessible.EventHandler;
-
 import app.Song;
-import javafx.beans.Observable;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
@@ -39,14 +29,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ListView;
-import javafx.scene.control.SelectionMode;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.scene.control.TextInputDialog;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.text.Text;
-import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
 
 /*
@@ -236,10 +219,6 @@ public class Controller extends ActionEvent {
 				songs = FXCollections.observableList(songList);
 				songPlayList.setItems(songs);
 				songPlayList.getSelectionModel().select(position);
-		        SDArtistBox.setText("");
-		        SDSongBox.setText("");
-		        SDAlbumBox.setText("");
-		        SDYearBox.setText("");
 			}
 		}
 		
